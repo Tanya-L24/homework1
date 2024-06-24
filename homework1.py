00b7cf5a96575396bdd887e11dad7593a0964c7a
Tanya-L24 <tanyalebed@inbox.ru>
2024-06-24 21:20:09 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+example = 'Interesting'
+print(example[0]) #первый символ этой строки
+print(example[-1]) #последний символ этой строки
+print(example[len(example)//2:]) #вторую половину этой строки
+print(example[::-1]) #это слово наоборот
+print(example[1::2]) #каждый второй символ этой строки
